ae6b4fdf2ccecb74e150705d07a61766adc50cf9
Austin Vigo <avigo@uci.edu>
2019-07-29 14:28:06 -0700

    Set up a project
    Setted up an xcode project

---
+++ MessagingApp/MessagingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessagingApp
+//
+//  Created by Austin Vigo on 7/29/19.
+//  Copyright © 2019 austin-keith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
