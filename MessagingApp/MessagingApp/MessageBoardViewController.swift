6864c5103897701d86aa7d0834d1d9c72a69fd03
Austin Vigo <avigo@uci.edu>
2019-08-01 18:11:19 -0700

    doing something
    doing something

--- MessagingApp/MessagingApp/MessageBoardViewController.swift
+++ MessagingApp/MessagingApp/MessageBoardViewController.swift
@@ -8,23 +8,33 @@
 
 import UIKit
 
-class MessageBoardViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class MessageBoardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var messagesTableView: UITableView!
+    
+    var messagesRecieved:[String] = []
+    func getAllMessages(){
+        messagesRecieved.append("austin.keith.vigo@gmail.com")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messagesRecieved.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = messagesRecieved[indexPath.row]
+        return cell
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        messagesTableView.delegate = self
+        messagesTableView.dataSource = self
+        getAllMessages()
+        
     }
-    */
+    
+
 
 }
